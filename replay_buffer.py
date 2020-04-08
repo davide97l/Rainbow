22670e870d4f1316b94970671de34feb1323e9da
Davide <davide97ls@gmail.com>
2020-04-08 10:32:10 +0800

    first commit

---
+++ replay_buffer.py
@@ -0,0 +1,257 @@
+from collections import deque
+from typing import Dict, List, Tuple
+import numpy as np
+import random
+from segment_tree import *
+
+
+class ReplayBuffer:
+    """A simple numpy replay buffer.
+    Parameters
+    ---------
+    obs_dim: int
+        Observation shape
+    size: int
+        # maximum number of elements in buffer
+    batch_size: int
+        batch_size
+    n_step: int
+        number of step used for N-step learning
+    gamma: float
+        gamma value
+    """
+
+    def __init__(
+            self,
+            obs_dim: int,
+            size: int = 1024,
+            batch_size: int = 32,
+            n_step: int = 1,
+            gamma: float = 0.99
+    ):
+        self.obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.next_obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.acts_buf = np.zeros([size], dtype=np.float32)
+        self.rews_buf = np.zeros([size], dtype=np.float32)
+        self.done_buf = np.zeros(size, dtype=np.float32)
+        self.max_size, self.batch_size = size, batch_size
+        self.ptr, self.size, = 0, 0
+
+        # for N-step Learning
+        self.n_step_buffer = deque(maxlen=n_step)
+        self.n_step = n_step
+        self.gamma = gamma
+
+    def store(
+            self,
+            obs: np.ndarray,
+            act: np.ndarray,
+            rew: float,
+            next_obs: np.ndarray,
+            done: bool,
+    ) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray, bool]:
+        """Store a new experience in the buffer"""
+        transition = (obs, act, rew, next_obs, done)
+        self.n_step_buffer.append(transition)
+
+        # single step transition is not ready
+        if len(self.n_step_buffer) < self.n_step:
+            return
+
+        # make a n-step transition
+        # take the n-reward, n-observation and n-done
+        rew, next_obs, done = self._get_n_step()
+        # take the 1-observation and 1-action
+        obs, act = self._get_first_step()
+
+        # store the transition
+        self.obs_buf[self.ptr] = obs
+        self.next_obs_buf[self.ptr] = next_obs
+        self.acts_buf[self.ptr] = act
+        self.rews_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+        return transition
+
+    def sample_batch(self) -> Dict[str, np.ndarray]:
+        """Sample a batch from the buffer"""
+        assert len(self) >= self.batch_size
+        idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
+
+        return dict(
+            obs=self.obs_buf[idxs],
+            next_obs=self.next_obs_buf[idxs],
+            acts=self.acts_buf[idxs],
+            rews=self.rews_buf[idxs],
+            done=self.done_buf[idxs],
+            # for N-step Learning
+            indices=idxs,  # # need this for priority updating
+        )
+
+    def sample_batch_from_idxs(
+        self, idxs: np.ndarray
+    ) -> Dict[str, np.ndarray]:
+        # for N-step Learning
+        return dict(
+            obs=self.obs_buf[idxs],
+            next_obs=self.next_obs_buf[idxs],
+            acts=self.acts_buf[idxs],
+            rews=self.rews_buf[idxs],
+            done=self.done_buf[idxs],
+        )
+
+    def _get_n_step(self) -> Tuple[np.int64, np.ndarray, bool]:
+        """Return n step rew, next_obs, and done."""
+        # info of the last transition
+        rew, next_obs, done = self.n_step_buffer[-1][-3:]
+
+        for transition in reversed(list(self.n_step_buffer)[:-1]):
+            r, n_o, d = transition[-3:]
+            # update the reward
+            rew = r + self.gamma * rew * (1 - d)
+            # if done == 1: next_obs is the first observation where done == 1
+            # if done == 0: next_obs is the n-observation
+            next_obs, done = (n_o, d) if d else (next_obs, done)
+
+        return rew, next_obs, done
+
+    def _get_first_step(self) -> Tuple[np.int64, np.ndarray]:
+        """Return first step obs and act."""
+        # info of the first transition
+        obs, act = self.n_step_buffer[0][:2]
+
+        return obs, act
+
+    def __len__(self) -> int:
+        return self.size
+
+
+class PrioritizedReplayBuffer(ReplayBuffer):
+    """Prioritized Replay buffer.
+    Attributes:
+        max_priority: float
+            max priority
+        tree_ptr: int
+            next index of tree
+        alpha: float
+            alpha parameter for prioritized replay buffer
+        sum_tree: SumSegmentTree
+            sum tree for prior
+        min_tree: MinSegmentTree
+            min tree for min prior to get max weight
+    """
+
+    def __init__(
+            self,
+            obs_dim: int,
+            size: int = 1024,
+            batch_size: int = 32,
+            alpha: float = 0.6,
+            n_step: int = 1,
+            gamma: float = 0.99,
+    ):
+        """Initialization."""
+        assert alpha >= 0
+
+        super(PrioritizedReplayBuffer, self).__init__(
+            obs_dim, size, batch_size, n_step, gamma
+        )
+        self.max_priority = 1.0
+        self.tree_ptr = 0
+        self.alpha = alpha
+
+        # capacity must be positive and a power of 2.
+        tree_capacity = 1
+        while tree_capacity < self.max_size:
+            tree_capacity *= 2
+
+        self.sum_tree = SumSegmentTree(tree_capacity)
+        self.min_tree = MinSegmentTree(tree_capacity)
+
+    def store(
+            self,
+            obs: np.ndarray,
+            act: int,
+            rew: float,
+            next_obs: np.ndarray,
+            done: bool,
+    ) -> Tuple[np.ndarray, np.ndarray, float, np.ndarray, bool]:
+        """Store an experience and its priority."""
+        transition = super().store(obs, act, rew, next_obs, done)
+
+        if transition:
+            self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
+            self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+
+        return transition
+
+    def sample_batch(self, beta: float = 0.4) -> Dict[str, np.ndarray]:
+        """Sample a batch of experiences."""
+        assert len(self) >= self.batch_size
+        assert beta > 0
+
+        # samples transitions indices
+        indices = self._sample_proportional()
+
+        obs = self.obs_buf[indices]
+        next_obs = self.next_obs_buf[indices]
+        acts = self.acts_buf[indices]
+        rews = self.rews_buf[indices]
+        done = self.done_buf[indices]
+        # importance sampling weights
+        weights = np.array([self._calculate_weight(i, beta) for i in indices])
+
+        return dict(
+            obs=obs,
+            next_obs=next_obs,
+            acts=acts,
+            rews=rews,
+            done=done,
+            weights=weights,
+            indices=indices,  # need this for priority updating
+        )
+
+    def update_priorities(self, indices: List[int], priorities: np.ndarray):
+        """Update priorities of sampled transitions."""
+        assert len(indices) == len(priorities)
+
+        for idx, priority in zip(indices, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self)
+
+            self.sum_tree[idx] = priority ** self.alpha
+            self.min_tree[idx] = priority ** self.alpha
+
+            self.max_priority = max(self.max_priority, priority)
+
+    def _sample_proportional(self) -> List[int]:
+        """Sample indices based on proportions."""
+        indices = []
+        p_total = self.sum_tree.sum(0, len(self))
+        segment = p_total / self.batch_size
+
+        # perform a random sample in each segment
+        for i in range(self.batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+            upperbound = random.uniform(a, b)
+            idx = self.sum_tree.find_prefixsum_idx(upperbound)
+            indices.append(idx)
+
+        return indices
+
+    def _calculate_weight(self, idx: int, beta: float):
+        """Calculate the weight of the experience at idx."""
+        # get max weight
+        p_min = self.min_tree.min() / self.sum_tree.sum()
+        max_weight = (p_min * len(self)) ** (-beta)
+
+        # calculate weights
+        p_sample = self.sum_tree[idx] / self.sum_tree.sum()
+        weight = (p_sample * len(self)) ** (-beta)
+        weight = weight / max_weight
+
+        return weight
---
+++ utils.py
@@ -0,0 +1,146 @@
+from torch import nn
+import torch
+import math
+import torch.nn.functional as F
+import numpy as np
+
+
+class NoisyLinear(nn.Module):
+    """Noisy linear module for NoisyNet.
+
+    Attributes:
+        in_features (int): input size of linear module
+        out_features (int): output size of linear module
+        std_init (float): initial std value
+        weight_mu (nn.Parameter): mean value weight parameter
+        weight_sigma (nn.Parameter): std value weight parameter
+        bias_mu (nn.Parameter): mean value bias parameter
+        bias_sigma (nn.Parameter): std value bias parameter
+    """
+
+    def __init__(
+            self,
+            in_features: int,
+            out_features: int,
+            std_init: float = 0.5,
+    ):
+        """Initialization."""
+        super(NoisyLinear, self).__init__()
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init  # used to initialize weight_sigma and bias_sigma
+
+        self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.weight_sigma = nn.Parameter(
+            torch.Tensor(out_features, in_features)
+        )
+
+        self.bias_mu = nn.Parameter(torch.Tensor(out_features))
+        self.bias_sigma = nn.Parameter(torch.Tensor(out_features))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def reset_parameters(self):
+        """Reset trainable network parameters (factorized gaussian noise)."""
+        mu_range = 1 / math.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(
+            self.std_init / math.sqrt(self.in_features)
+        )
+        self.bias_mu.data.uniform_(-mu_range, mu_range)
+        self.bias_sigma.data.fill_(
+            self.std_init / math.sqrt(self.out_features)
+        )
+
+    def reset_noise(self):
+        """Generate new noise and make epsilon matrices."""
+        epsilon_in = self.scale_noise(self.in_features)
+        epsilon_out = self.scale_noise(self.out_features)
+
+        # outer product
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        self.bias_epsilon.copy_(epsilon_out)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Add noise to the parameters to perform the forward step"""
+        return F.linear(
+            x,
+            self.weight_mu + self.weight_sigma * self.weight_epsilon,
+            self.bias_mu + self.bias_sigma * self.bias_epsilon,
+        )
+
+    @staticmethod
+    def scale_noise(size: int) -> torch.Tensor:
+        """Generate noise (factorized gaussian noise)."""
+        x = torch.FloatTensor(np.random.normal(loc=0.0, scale=1.0, size=size))
+        return x.sign().mul(x.abs().sqrt())
+
+
+class Network(nn.Module):
+    """
+
+    Attributes:
+        in_dim (int): input size
+        out_dim (int): output size
+        atom_size (int): atom size, used to compute categorical distribution
+        support (torch.Tensor): discrete support z
+    """
+    def __init__(
+            self,
+            in_dim: int,
+            out_dim: int,
+            atom_size: int,
+            support: torch.Tensor
+    ):
+        """Initialization."""
+        super(Network, self).__init__()
+
+        self.support = support
+        self.out_dim = out_dim
+        self.atom_size = atom_size
+
+        # set common feature layer
+        self.feature_layer = nn.Sequential(
+            nn.Linear(in_dim, 128),
+            nn.ReLU(),
+        )
+
+        # set advantage layer
+        self.advantage_hidden_layer = NoisyLinear(128, 128)
+        self.advantage_layer = NoisyLinear(128, out_dim * atom_size)  # output one distribution per action
+
+        # set value layer
+        self.value_hidden_layer = NoisyLinear(128, 128)
+        self.value_layer = NoisyLinear(128, atom_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        dist = self.dist(x)
+        q = torch.sum(dist * self.support, dim=2)
+        return q
+
+    def dist(self, x: torch.Tensor) -> torch.Tensor:
+        """Get distribution for atoms."""
+        feature = self.feature_layer(x)
+        adv_hid = F.relu(self.advantage_hidden_layer(feature))
+        val_hid = F.relu(self.value_hidden_layer(feature))
+
+        advantage = self.advantage_layer(adv_hid).view(
+            -1, self.out_dim, self.atom_size
+        )
+        value = self.value_layer(val_hid).view(-1, 1, self.atom_size)
+        q_atoms = value + advantage - advantage.mean(dim=1, keepdim=True)
+
+        dist = F.softmax(q_atoms, dim=-1)
+        dist = dist.clamp(min=1e-3)  # for avoiding nans
+
+        return dist
+
+    def reset_noise(self):
+        """Reset all noisy layers."""
+        self.advantage_hidden_layer.reset_noise()
+        self.advantage_layer.reset_noise()
+        self.value_hidden_layer.reset_noise()
+        self.value_layer.reset_noise()
