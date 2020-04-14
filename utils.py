a7ab8f0b96ddd6b338817b5f6022ecc83209c032
Davide <davide97ls@gmail.com>
2020-04-14 21:52:50 +0800

    changed convnet architecture

--- utils.py
+++ utils.py
@@ -196,21 +196,21 @@ class ConvNet(nn.Module):
         self.atom_size = atom_size
 
         # conv layer (input at least 1 x 1 x 40 x 40)
-        self.conv1 = nn.Conv2d(in_dim[2], 8, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(8)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(in_dim[2], 16, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(32)
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size=5, stride=2):
+        def conv2d_size_out(size, kernel_size=8, stride=4):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        conv_w = conv2d_size_out(conv2d_size_out(conv2d_size_out(in_dim[1])))
-        conv_h = conv2d_size_out(conv2d_size_out(conv2d_size_out(in_dim[0])))
-        self.hidden_size = conv_w * conv_h * 16
+        conv_w = conv2d_size_out(conv2d_size_out(conv2d_size_out(in_dim[1], 8, 4), 4, 2), 3, 1)
+        conv_h = conv2d_size_out(conv2d_size_out(conv2d_size_out(in_dim[0], 8, 4), 4, 2), 3, 1)
+        self.hidden_size = conv_w * conv_h * 32
 
         # set advantage layer
         self.advantage_hidden_layer = NoisyLinear(self.hidden_size, self.hidden_size)
